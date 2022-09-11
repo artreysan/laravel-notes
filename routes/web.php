b3e1561e53ab0bcd1041088145b1bf876607aebc
artreysan <al2173038770@azc.uam.mx>
2022-09-11 01:26:30 -0500

    Routes 1.1

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,31 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+Here definite the routes in the app.
 */
 
+// Access to page http://127.0.0.1:8000/
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');//The view is in ./resources/views/welcome.blade.php
 });
+
+// Access to page http://127.0.0.1:8000/cursos
+Route::get('cursos',function(){
+    return "Pagina cursos";
+});
+
+// Access to page http://127.0.0.1:8000/cursos/create
+// Access to page http://127.0.0.1:8000/cursos/delete
+// {create} usa el parametro de la funcion
+Route::get('cursos/{curso}',function($curso){
+    return "BIenvenido al curso: $curso";
+});
+
+//SIempre se ejecutara el arriba primero dado que es secuencial
+Route::get('cursos/create',function(){
+    return "BIenvenido al curso: create 2";
+});
+
+
+
+
